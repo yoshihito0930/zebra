9f2cff690f65cec830d3956872713bd4903eb632
Yoshihito Ishihara <yoshihito.093079@gmail.com>
2025-12-20 15:04:59 +0900

    画像表示のパフォーマンス改善

--- studio-website/astro/astro.config.mjs
+++ studio-website/astro/astro.config.mjs
@@ -6,4 +6,12 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://studio-zebra.com', // 本番環境のURL（要変更）
   integrations: [sitemap()],
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp',
+      config: {
+        limitInputPixels: false,
+      }
+    },
+  },
 });
